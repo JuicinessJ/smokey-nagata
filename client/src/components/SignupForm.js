b7b4a70052f455655f8f387580a3fd380f37b3a7
JoshuaHamann <joshwashku@yahoo.com>
2023-04-07 16:30:53 -0500

    added some intial states and mutations for adding a user and handling signup data

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,5 +1,33 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
+import { ADD_USER } from '../utils/auth';
 import Auth from '../utils/auth';
 import { useMutation } from '@apollo/client';
+
+const SignupForm = () => {
+
+    // set initial form state with username, email, and password
+    const [signupFormData, setSignupFormData] = useState({ username: '', email: '', password: ''});
+    // set state for form validation
+    const [validated] = useState(false);
+
+    // use a mutation to add users
+    const [addUser, { error }] = useMutation(ADD_USER);
+    
+    const handleSignupInput = (event) => {
+        const { name, value } = event.target;
+        setSignupFormData({ ...signupFormData, [name]: value});
+    };
+
+    const handleFormSubmit = async (event) => {
+
+    }
+    
+    return (
+        <>
+        </>
+    );
+};
+
+export default SignupForm
+
