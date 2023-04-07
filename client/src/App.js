20eeeb4c8e6a868f091d4b7d00c84882df241ca8
JoshuaHamann <joshwashku@yahoo.com>
2023-04-06 20:38:35 -0500

    added the client with the auth to our apolloprovider

--- client/src/App.js
+++ client/src/App.js
@@ -31,6 +31,7 @@ const client = new ApolloClient({
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -47,6 +48,7 @@ function App() {
         </a>
       </header>
     </div>
+    </ApolloProvider>
   );
 }
 
