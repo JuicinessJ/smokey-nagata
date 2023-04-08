3b977186e9aaf247cf667264b4f020599b4ba805
abasheryassin <abasheryassin@gmail.com>
2023-04-08 15:39:54 -0500

    typedefs added and spelling error

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,28 @@
 const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        password: String!
+        location: String!
+        cars: [Post]
+    }
+
+    type Post {
+        _id: ID!
+        make: String!
+        model: String!
+        year: Int!
+        color: String
+        mileage: Int
+        createdAt: Date
+        username: String!
+    }
+
+    type Auth {
+        token: ID!
+        user: User!
+    }
+`;
