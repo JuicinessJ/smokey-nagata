544bd2f585be4e7694e8c595b4acf193f5f50c9a
Your Name <berglundgibson@gmail.com>
2023-04-09 17:41:47 -0400

    some css on content page

--- client/src/pages/Content.js
+++ client/src/pages/Content.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import BidForm from '../components/Bidform'
+import CarPic from '../assets/images/lincoln-continental.jpg'
 
 const index = () => {
   return (
     <div className='maincontent'>
         <h1 className='sitetitle'>This Car In This Area</h1>
-        <div className='imagecontainer'>
-            <img className='carpic' src='./assets/images/1964-lincoln-continental.jpg' alt='car for sale'></img>
-            <p className='cardescription'></p>
+        <div className='picandbidform'>
+            <div className='imagecontainer'>
+                <img className='carpic' src={CarPic} alt='car for sale'></img>
+                <p className='cardescription'></p>
+            </div>
+            <div className='bidformcontainer'>
+                <BidForm />
+            </div>
         </div>
-        <BidForm />
         <div className='posterinfo'>
             <p>This car was posted by User</p>
             <p>User.Location</p>
