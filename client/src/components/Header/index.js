550a93528b8142ea93527f4d616bc93df10e6133
Jesse <JuicinessJMN@Gmail.com>
2023-04-09 21:28:03 -0500

    finished header, copied from 21-26

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,8 +1,45 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Auth from '../../utils/auth';
 
 const Header = () => {
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
+
   return (
-    <div>index</div>
+    <header>
+      <div>
+        <div>
+          <Link className='' to="/">
+            <h1>The Car's Shop</h1>
+          </Link>
+        </div>
+        <div>
+          {Auth.loggedIn() ? (
+            <>
+              <Link className='' to="/me">
+                {Auth.getProfile().data.username}'s profile
+              </Link>
+              <button className='' onClick={logout}>
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              <Link className='' to="/login"> 
+                Login
+              </Link>
+              <Link className='' to="/signup">
+                Signup
+              </Link>
+            </>
+          )}
+        </div>
+      </div>
+    </header>
   )
 }
 
