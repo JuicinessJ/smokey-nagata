f45618282e86102dd13de035790d4ded45c61240
Jesse <JuicinessJMN@Gmail.com>
2023-04-10 17:40:00 -0500

    added some basics into login.js

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+
+import Auth from '../utils/auth';
 
 const Login = () => {
+  const [formState, setFormState] = useState({ email: '', password: ''});
+  const [login, { error, data }] = useMutation(LOGIN_USER);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({...formState, [name]: value});
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+    try {
+      const { data } = await login({ variables: { ...formState } });
+
+      Auth.login(data.login.token);
+    } catch (err) {
+      console.log(err);
+    }
+
+
+    setFormState({email: '', password: ''});
+  };
+
   return (
-    <div>Login</div>
+    <main>
+      <div>
+        <div>
+          <h4>Login</h4>
+          <div>
+            {/* Add formSubmit */}
+          </div>
+        </div>
+      </div>
+    </main>
   )
 }
 
