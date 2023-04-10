3807a946d55d3eaa6fd0bc616eafed771f601ebc
Jesse <JuicinessJMN@Gmail.com>
2023-04-09 19:31:09 -0500

    commented out homepage and signupform inside components

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,49 +1,49 @@
-import React, { useState } from 'react';
-//import PortfolioItems from './Pages/PortfolioItems';
-//import PortfolioNav from './PortfolioNav';
-//import AboutMe from './Pages/AboutMe';
-//import ContactMe from './Pages/ContactMe';
-import Homepage from './Pages/Homepage';
+// import React, { useState } from 'react';
+// //import PortfolioItems from './Pages/PortfolioItems';
+// //import PortfolioNav from './PortfolioNav';
+// //import AboutMe from './Pages/AboutMe';
+// //import ContactMe from './Pages/ContactMe';
+// import Homepage from './Pages/Homepage';
 
-function DisplayPage() {
-    const [currentPage, setCurrentPage] = useState('Homepage');
+// function DisplayPage() {
+//     const [currentPage, setCurrentPage] = useState('Homepage');
   
-    // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
-    const renderPage = () => {
-      if (currentPage === 'Login/Signup') {
-        return <LoginSignUp />;
-      }
-      if (currentPage === 'MyProfile') {
-        return <MyProfile />;
-      }
-      return <Homepage />;
-    }
-
-const handlePageChange = (page) => setCurrentPage(page);
-
-// let submitBtn = document.querySelector('.submitbtn');
-//     let userEmail = document.querySelector('useremail');
-
-//     function displayEmailError() {
-//         userEmail.placeholder = 'Please enter a valid email address';
-//         userEmail.setAttribute('style', 'color: red;');
+//     // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+//     const renderPage = () => {
+//       if (currentPage === 'Login/Signup') {
+//         return <LoginSignUp />;
+//       }
+//       if (currentPage === 'MyProfile') {
+//         return <MyProfile />;
+//       }
+//       return <Homepage />;
 //     }
 
-//     submitBtn.addEventListener('click', function formSubmit() {
-//         if(userEmail.value === /.*@.*.(com|net)/) {
-//             return;
-//         } else {
-//             displayEmailError();
-//         };
-//     }
-//     );
+// const handlePageChange = (page) => setCurrentPage(page);
 
-return (
-    <div>
-        <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />
-        {renderPage()}
-    </div>
-    )
-};
+// // let submitBtn = document.querySelector('.submitbtn');
+// //     let userEmail = document.querySelector('useremail');
 
-export default DisplayPage;
+// //     function displayEmailError() {
+// //         userEmail.placeholder = 'Please enter a valid email address';
+// //         userEmail.setAttribute('style', 'color: red;');
+// //     }
+
+// //     submitBtn.addEventListener('click', function formSubmit() {
+// //         if(userEmail.value === /.*@.*.(com|net)/) {
+// //             return;
+// //         } else {
+// //             displayEmailError();
+// //         };
+// //     }
+// //     );
+
+// return (
+//     <div>
+//         <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />
+//         {renderPage()}
+//     </div>
+//     )
+// };
+
+// export default DisplayPage;
