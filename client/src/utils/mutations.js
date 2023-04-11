c5e335ef5445c5e9489da5f923c03604eda7c4ea
abasheryassin <abasheryassin@gmail.com>
2023-04-10 21:45:22 -0500

    client side add user and login user mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -2,24 +2,28 @@ import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
+  login(email: $email, password: $password) {
+    token
+    user {
+      _id
+      email
+      password
     }
   }
+}
 `;
 
 
 export const ADD_USER = gql`
-  mutation addUser($username: String!) {
-    addUser(username: $username) {
+  mutation addUser($username: String!, $email: String!, $password: String!, $location: String!) {
+  addUser(username: $username, email: $email, password: $password, location: $location) {
+    token
+    user {
+      _id
       email
-      username
       password
     }
+  }
 }
 `;
 
