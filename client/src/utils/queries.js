eaaa204ca5435a0a326f61f586dc21ddbb0c9536
JoshuaHamann <joshwashku@yahoo.com>
2023-04-12 16:09:34 -0500

    added query_me

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,20 @@
-// import { gql } from '@apollo/client';
+import { gql } from '@apollo/client';
 
+
+export const QUERY_ME = gql`
+        query me {
+            me {
+                _id
+                cars {
+                _id
+                }
+                email
+                location
+                password
+                username
+            }
+        }
+`;
 // export const QUERY_PROFILES = gql`
 //     query allProfiles {
 //         profiles {
