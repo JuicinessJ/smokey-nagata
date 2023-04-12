7e875b5a0caf4ec72372e311a1e032e8487c3564
abasheryassin <abasheryassin@gmail.com>
2023-04-11 19:17:49 -0500

    add password verification

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -26,20 +26,3 @@ export const ADD_USER = gql`
   }
 }
 `;
-
-
-/* Don't know if it should be like this versus that.
-
-export const ADD_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
-    }
-  }
-`;
-
-*/
