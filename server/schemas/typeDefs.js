e160f0742f4a98cf0f602ec5c34930d9aed7cf49
JoshuaHamann <joshwashku@yahoo.com>
2023-04-12 20:28:28 -0500

    added some Yassins typeDef

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -36,8 +36,10 @@ const typeDefs = gql`
     }
 
     type Query {
+        users: [User]
         me: User
         posts(username: String): [Post]
+        post(postId: ID!): Post
     }
 
     type Mutation {
