11e87f84ca754a11dad37ac3a5451d0f8532ee85
Jesse <JuicinessJMN@Gmail.com>
2023-04-12 21:46:20 -0500

    added stuff into PostForm

--- client/src/components/PostForm/index.js
+++ client/src/components/PostForm/index.js
@@ -2,33 +2,63 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 
-import { ADD_CONTENT } from '../../utils/mutations';
+import { ADD_POST } from '../../utils/mutations';
 import { QUERY_CONTENT, QUERY_ME } from '../../utils/queries';
 
-import Auth from '../../utils/auth';
+// import Auth from '../../utils/auth';
 
 
-const ContentForm = () => {
-  const [contentText, setcontentText] = useState('');
+const PostForm = () => {
+  const [formState, setFormState] = useState({
+    make: '',
+    model: '',
+    year: '',
+    color: '',
+    mileage: ''
+  })
 
+  const [addPost, {error}] = useMutation(ADD_POST);
 
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
 
-
-
-
-
-
-
-
-
-
-
-
-
+    try {
+      const { data } = await addPost({
+        variables: {
+          ...formState
+        }
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
-    <div>index</div>
+    <div>
+      <h3>Add Your Car</h3>
+      <form id='carForm' onSubmit={handleFormSubmit}>
+
+        <div>
+          <label>The Make:</label>
+          <input type='text' name='make' id='carMake'></input>
+
+          <label>The Model:</label>
+          <input type='text' name='model' id='carModel'></input>
+
+          <label>The Year:</label>
+          <input type='text' name='year' id='carYear'></input>
+
+          <label>The Color:</label>
+          <input type='text' name='color' id='carColor'></input>
+
+          <label>The Mileage:</label>
+          <input type='text' name='mileage' id='carMileage'></input>
+        </div>
+
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
   )
 }
 
-export default ContentForm
+export default PostForm;
